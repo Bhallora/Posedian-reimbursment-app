81dc446e254c189e91cc1415974db5286eceab7c
Pallab Borah <pallab.borah@kdi.kongsberg.com>
2019-09-23 15:15:39 +0530

    add first page style

--- src/components/wizard-form/wizard-first-page/WizardFormFirstPage.js
+++ src/components/wizard-form/wizard-first-page/WizardFormFirstPage.js
@@ -4,31 +4,44 @@ import validate from '../validate/validate'
 import renderField from '../render-field/renderField'
 import TextBox from '../../text-box/TextBox';
 import NextButton from '../../next-button/NextButton';
+import './wizard-form-first-page.scss';
+import KxCard from '../../card/card';
 
 const WizardFormFirstPage = props => {
   const { handleSubmit } = props
   return (
     <form onSubmit={handleSubmit}>
-      <TextBox
-        name="name"
-        label="Name"
-        type="text"
-        component={renderField}
-      //  label="Name"
-      />
-      <TextBox
-        label="Employee Code"
-        type="text"
-        component={renderField}
-        // label="Employee Code"
-        name="employeeCode"
-      />
-      <div>
-        <NextButton className="next" />
+      <div className="flex-container-page-1">
+        <KxCard className="flex-KxCard-page-1">
+          <div className="flex-form-content-and-button">
+            <div> <p className="heading">Basic Information </p></div>
 
 
+            <TextBox
+              name="name"
+              label="Name"
+              type="text"
+              component={renderField}
+            //  label="Name"
+            />
+            <TextBox
+              label="Employee Code"
+              type="text"
+              component={renderField}
+              // label="Employee Code"
+              name="employeeCode"
+            />
+
+            <div className="flex-button-container-page-1">
+              <NextButton className="next" />
+
+
+            </div>
+          </div>
+        </KxCard>
       </div>
     </form>
+
   )
 }
 
